4f67429df1ef991c7aaec55c1b0fecbdaef5183d
Student <student@macbook-pro-0419-knezevic.local>
2017-02-23 10:48:43 -0500

    working on project

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -23,7 +23,20 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this comment and add your code below...
+
+for y in stride(from: 15, through: 50, by: 1){
+    canvas.drawLine(fromX: 25, fromY:50 , toX: 275, toY: y)
+}
+
+for x in stride(from: 130, through: 200, by: 1){
+    canvas.drawLine(fromX: 250, fromY: 10, toX: x, toY: 235)
+}
+
+
+
+
+
+
 
 /*:
  ## Template code
